67c23e70f9a855f39f9ee2d320f22fbdecf35163
Tanmay <tanmay@tanmay.me>
2025-03-01 21:14:19 +0530

    Initial Commit

---
+++ DeleteFeedbackQuickAction/DeleteFeedbackQuickActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeleteFeedbackQuickActionApp.swift
+//  DeleteFeedbackQuickAction
+//
+//  Created by Tanmay Sonawane on 01/03/25.
+//
+
+import SwiftUI
+
+@main
+struct DeleteFeedbackQuickActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
